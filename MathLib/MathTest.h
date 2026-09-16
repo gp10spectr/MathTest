2b66c7dde76e514a74c8742d87696ab6d7ea9fe9
Khaferl Pavel <khaferpavel@gmail.com>
2026-09-17 01:08:47 +0300

    extended version of MathTest.h

--- MathLib/MathTest.h
+++ MathLib/MathTest.h
@@ -1,16 +1,44 @@
 #pragma once
+#include "Task.h"
+#include <string>
 
+// mark A/B/C/D/F
+// A >= 90%
+// B 80 - 89%
+// C 70 - 79%
+// D 60 - 69%
+// F < 60%
 class MathTest {
 private:
-	//task field
-	int count;
-	int* user_answers;
-	int correct_count;
+    Task* tasks;
+    int   count;
+    int* user_answers;
+    int   correct_count;
+    bool* answered;
+
+    void init(int count, int min, int max, char operation);
+
 public:
-	//constructor (init with size, init with size and range, init with size and range and operation)
-	//deconstructor
-	//run()
-	//init_test()
-	//show_statistics(tab with user answers and real answers)
-	//show_question(ask user questions)
+    MathTest(int start_count);
+    MathTest(int count, int min, int max);
+    MathTest(int count, int min, int max, char operation);
+    ~MathTest();
+
+    //without that there might be a conflict with default constructor
+    MathTest(const MathTest&) = delete;
+    MathTest& operator=(const MathTest&) = delete;
+
+    void run();
+    void show_statistics();
+
+    void submit_answer(int index, int answer);
+    std::string question_text(int index) const;
+    std::string build_statistics() const;
+    char get_mark() const;
+
+    int get_count() const;
+    int get_correct_count() const;
+    const Task& get_task(int index) const;
+    int get_user_answer(int index) const;
+    bool is_answered(int index) const;
 };
